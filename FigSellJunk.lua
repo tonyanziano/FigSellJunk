2c365fbe2d6723d22900196c11145424ef24fb89
Tony <tonyanziano5@gmail.com>
2022-12-07 19:03:42 -0800

    Fixed an issue where selling items too fast was stopping some items from being sold.

--- FigSellJunk.lua
+++ FigSellJunk.lua
@@ -1,6 +1,25 @@
 local maxNumBags = 4
 local function sellJunk()
+  local vendorItemTicker
+  local itemsToSell = {}
   local numJunkItemsSold = 0
+
+  local function sellItem()
+    if table.getn(itemsToSell) > 0 then
+      -- sell an item from the list
+      local itemIndices = table.remove(itemsToSell)
+      local bagIndex = itemIndices[1]
+      local slotIndex = itemIndices[2]
+      C_Container.UseContainerItem(bagIndex, slotIndex)
+      numJunkItemsSold = numJunkItemsSold + 1
+    else
+      -- clean up and report
+      vendorItemTicker:Cancel()
+      if numJunkItemsSold > 0 then
+        print(format('<FigSellJunk> sold %i items', numJunkItemsSold))
+      end
+    end
+  end
 
   -- iterate through every bag
   for bagIndex = 0, maxNumBags do
@@ -11,16 +30,15 @@ local function sellJunk()
       for slotIndex = 1, numSlotsInBag do
         local itemInfo = C_Container.GetContainerItemInfo(bagIndex, slotIndex)
         if itemInfo ~= nil and itemInfo.quality == Enum.ItemQuality.Poor then
-          -- vendor the item
-          C_Container.UseContainerItem(bagIndex, slotIndex)
-          numJunkItemsSold = numJunkItemsSold + 1
+          -- mark the item to be vendored
+          table.insert(itemsToSell, { bagIndex, slotIndex })
         end
       end
     end
   end
-  if numJunkItemsSold > 0 then
-    print(format('<FigSellJunk> sold %i items', numJunkItemsSold))
-  end
+
+  -- every 0.1 seconds, vendor an item (this avoids some items becoming busy due to trying to sell too fast)
+  vendorItemTicker = C_Timer.NewTicker(0.1, sellItem)
 end
 
 local function onEvent(frame, event, ...)
@@ -31,7 +49,7 @@ local function onEvent(frame, event, ...)
       local merchantFrame = _G['MerchantFrame']
       sellJunkBtn = CreateFrame('button', 'FigSellJunkButton', merchantFrame, 'UIPanelButtonTemplate')
       sellJunkBtn:SetSize(80, 30)
-      sellJunkBtn:SetText('Sell Junk');
+      sellJunkBtn:SetText('Sell Junk')
       sellJunkBtn:SetPoint('TOPRIGHT', merchantFrame, 'BOTTOMRIGHT')
       sellJunkBtn:SetScript('OnClick', sellJunk)
     end
